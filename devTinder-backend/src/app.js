5387b724661b91ccbf710c82d86abb08a8b10529
beyou1298 <gaurav120998chachada@gmail.com>
2025-03-15 22:19:18 +0530

    ALL APIs calls GET,POST,PUSH,PATCH,DELETE

--- devTinder-backend/src/app.js
+++ devTinder-backend/src/app.js
@@ -1,45 +1,57 @@
 const express = require("express");
-//USING app.use
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("This is Hello Page from backend");
-});
+//USING app.use
 
-app.use("/test", (req, res) => {
-  res.send("This is Test Page from backend");
-});
-
-app.use("/",(req, res) => {
-  res.send("Welcome to the DEVTINDER Backend");
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
-
-
-//GET CALLS
-// const express = require("express");
-
-// const app = express();
-
-// // Handle use requests to the root route
-// app.get("/", (req, res) => {
-//   res.send("Welcome to the DEVTINDER Backend");
-// });
-
-// // Handle GET requests to /test route
-// app.get("/test", (req, res) => {
-//   res.send("This is Test Page from backend");
-// });
-
-// // Handle GET requests to /hello route
-// app.get("/hello", (req, res) => {
+// app.use("/hello", (req, res) => {
 //   res.send("This is Hello Page from backend");
 // });
 
-// // Start the server
-// app.listen(3000, () => {
-//   console.log("Server is running on http://localhost:3000");
+// app.use("/test", (req, res) => {
+//   res.send("This is Test Page from backend");
 // });
+
+// app.use("/",(req, res) => {
+//   res.send("Welcome to the DEVTINDER Backend");
+// });
+
+// ============API CALLS================
+
+//If i use below code here so it will always give response as HAHAHA for all user apis
+//This happens because of order in which route written:
+/*
+app.use("/user", (req, res) => {
+  res.send("HAHAHHAHAA");
+});
+*/
+
+//GET
+app.get("/user", (req, res) => {
+  console.log("INSIDE GET USER API");
+  res.send({ firstName: "Gaurav", lastName: "Chachada" });
+});
+//POST
+app.post("/user", (req, res) => {
+  console.log("INSIDE POST USER API");
+  res.send("Data is saved successfully in USER Dbs");
+});
+//DELETE
+app.delete("/user", (req, res) => {
+  console.log("INSIDE DELET USER API");
+  res.send("Data is deleted successfully from USER Dbs");
+});
+
+app.put("/user", (req, res) => {
+  console.log("INSIDE PUT USER API");
+  res.send("Data is udpated successfully from USER Dbs");
+});
+
+app.patch("/user", (req, res) => {
+  console.log("INSIDE PATCH USER API");
+  res.send("PATCH API");
+});
+
+// // Start the server
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
